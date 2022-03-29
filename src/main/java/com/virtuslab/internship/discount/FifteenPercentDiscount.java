104be97727ac6db89ddf9b4a159cb74b9be7335e
Aleksandra Rojek <rojek.aleksandra1@gmail.com>
2022-03-29 23:12:45 +0200

    Implement FifteenPercentDiscount class

---
+++ src/main/java/com/virtuslab/internship/discount/FifteenPercentDiscount.java
@@ -0,0 +1,25 @@
+package com.virtuslab.internship.discount;
+
+import com.virtuslab.internship.product.Product;
+import com.virtuslab.internship.receipt.Receipt;
+
+import java.math.BigDecimal;
+
+public class FifteenPercentDiscount {
+
+    public static String NAME = "FifteenPercentDiscount";
+
+    public Receipt apply(Receipt receipt) {
+        if (shouldApply(receipt)) {
+            var totalPrice = receipt.totalPrice().multiply(BigDecimal.valueOf(0.85));
+            var discounts = receipt.discounts();
+            discounts.add(NAME);
+            return new Receipt(receipt.entries(), discounts, totalPrice);
+        }
+        return receipt;
+    }
+
+    private boolean shouldApply(Receipt receipt) {
+        return receipt.entries().stream().filter(rc -> rc.product().type() == Product.Type.GRAINS).mapToInt(rc -> rc.quantity()).sum() >=3;
+    }
+}
