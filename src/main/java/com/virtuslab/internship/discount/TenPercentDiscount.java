0b4993b31256cc3e97ecc95b0a12f354722d06d9
Aleksandra Rojek <rojek.aleksandra1@gmail.com>
2022-03-29 23:07:51 +0200

    Fix TenPercentDiscountTest

--- src/main/java/com/virtuslab/internship/discount/TenPercentDiscount.java
+++ src/main/java/com/virtuslab/internship/discount/TenPercentDiscount.java
@@ -19,6 +19,6 @@ public class TenPercentDiscount {
     }
 
     private boolean shouldApply(Receipt receipt) {
-        return receipt.totalPrice().compareTo(BigDecimal.valueOf(50)) < 0;
+        return receipt.totalPrice().compareTo(BigDecimal.valueOf(50)) >= 0;
     }
 }
