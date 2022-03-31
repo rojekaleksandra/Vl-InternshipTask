8ca7cb74c731a84bce515ad0e3a222753a588a81
Aleksandra Rojek <rojek.aleksandra1@gmail.com>
2022-03-31 21:19:23 +0200

    Implement BasketController class

--- src/main/java/com/virtuslab/internship/basket/Basket.java
+++ src/main/java/com/virtuslab/internship/basket/Basket.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Component
 @RequestScope
-public final class Basket {
+public class Basket {
 
     private final List<Product> productsBasket;
 
---
+++ src/main/java/com/virtuslab/internship/controller/BasketController.java
@@ -0,0 +1,53 @@
+package com.virtuslab.internship.controller;
+
+import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
+import com.virtuslab.internship.product.ProductDb;
+import com.virtuslab.internship.receipt.Receipt;
+import com.virtuslab.internship.receipt.ReceiptGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class BasketController {
+    private final ReceiptGenerator receiptGenerator;
+    private final ProductDb productDb;
+    private final Basket basket;
+
+    @Autowired
+    public BasketController(final ReceiptGenerator receiptGenerator,
+                            final ProductDb productDb,
+                            final Basket basket) {
+        this.receiptGenerator = receiptGenerator;
+        this.productDb = productDb;
+        this.basket = basket;
+    }
+
+    @GetMapping("/calculate")
+    public ResponseEntity<String> calculatePrice(@RequestParam List<String> productNames) {
+        final List<Product> products = productNames.stream()
+                .map(productDb::getProduct)
+                .collect(Collectors.toList());
+
+        basket.addProducts(products);
+
+        final Receipt receipt = receiptGenerator.generate(basket);
+
+        String result = productNames.stream()
+                .map( p -> productDb.getProduct(p).name() + " " +  productDb.getProduct(p).price())
+                .collect(Collectors.joining(", "));
+
+        if(receipt.discounts().size()>0)
+            result += " -> " + receipt.discounts().stream().collect(Collectors.joining(", "));
+
+        result += " = " + receipt.totalPrice();
+
+        return ResponseEntity.ok(result);
+    }
+}
